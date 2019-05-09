4a33182296eb2f8a23fae617fb494363d0eac62b
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-09 15:11:28 +0100

    Create Person class and Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(personnel) {
+    this.name = personnel.name;
+    this.age = personnel.age;
+    this.location = personnel.location;
+    this.gender = personnel.gender;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(personnel) {
+    super(personnel);
+    this.speciality = personnel.speciality;
+    this.favLanguage = personnel.favLanguage;
+    this.catchPhrase = personnel.catchPhrase;
+  }
+  demo() {
+    return `Today we are learning about ${this.subject}`;
+  }
+  grade() {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
